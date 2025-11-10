ad6ab2425a0de28e4e76901d0e5807b74e65efc7
MagnusHSundby <Magnus.sundby2@gmail.com>
2025-11-10 13:16:31 +0100

    html type shi

--- enturBus.js
+++ enturBus.js
@@ -14,7 +14,7 @@ async function fetchDepartures() {
             stopPlace(id: "NSR:StopPlace:6435") {
               id
               name
-              estimatedCalls(timeRange: 72100, numberOfDepartures: 10) {
+              estimatedCalls(timeRange: 72100, numberOfDepartures: 12) {
                 realtime
                 aimedArrivalTime
                 expectedArrivalTime
@@ -55,27 +55,33 @@ fetchDepartures()
 
 
 function showDepartures() {
-  //document.getElementById("showText").innerHTML = "aimed arrival: " + departuresData.data.stopPlace.estimatedCalls[0].aimedArrivalTime;
-  //document.getElementById("showText2").innerHTML = "expected arrival: " + departuresData.data.stopPlace.estimatedCalls[0].expectedArrivalTime;
 
-  for (let call of departuresData.data.stopPlace.estimatedCalls) {
-  let aimedArrival = call.aimedArrivalTime;
+  const container = document.getElementById("departures");
+  const calls = departuresData.data.stopPlace.estimatedCalls
 
+  container.innerHTML = "";
+  for (let call of calls) {
+  // let aimedArrival = call.aimedArrivalTime; 
+  // will be used to show how late a bus is  
   let expectedArrival = new Date(call.expectedArrivalTime);
   let now = new Date()
-  let timeDiff = Math.floor((expectedArrival - now) / 1000 / 60)
-
-  if (timeDiff <= 0) {
-    timeDiff = "nå"
-  }
+  let timeDiff = Math.floor((expectedArrival - now) / 1000 / 60) 
 
   let frontText = call.destinationDisplay.frontText;
   let lineId = call.serviceJourney.journeyPattern.line.id.split(":").pop()
+
+  const div = document.createElement("div");
+  div.classList.add("departures");
+  div.innerHTML = `
+    <span class="line">${lineId} ${frontText}</span>
+    <span class="time">${timeDiff <= 0 ? "nå" : timeDiff + " min"}</span>
+  `;
+  container.appendChild(div);
   
   
   console.log(`${lineId} ${frontText}: ${timeDiff}`)
   }
-    console.log("-------------------------")
+  console.log("-------------------------")
 }
 
 
